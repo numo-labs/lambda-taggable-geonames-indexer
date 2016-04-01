c367626203051a1706f41666f37da1de26b38e51
nelsonic <contact.nelsonic+github@gmail.com>
2016-04-01 16:15:40 +0100

    adds geonames_hierarchy method to lookup parent geotags

--- test/geonames.test.js
+++ test/geonames.test.js
@@ -2,17 +2,34 @@ var assert = require('assert');
 var geonames = require('../lib/geonames');
 var fixtures = require('./hotels_fixture.json');
 
-describe('Geonames find nearby place by Lat/Long', function () {
-  it('geonames.find', function (done) {
+describe('Geonames API', function () {
+  it('geonames.find (find_nearby_place_name_by_lat_lng)', function (done) {
     // the fixtures have commas in the Lat/Long values ...
     var lat = fixtures[0].Latitude;
     var lng = fixtures[0].Longitude;
     geonames.find(lat, lng, function (err, data) {
       // console.log(err, data);
       assert(!err, 'No erros');
-      console.log('geonameId:', data.geonames[0].geonameId);
+      // console.log('geonameId:', data.geonames[0].geonameId);
       assert(data.geonames[0].geonameId === 3343565);
       done();
+    });
+  });
+
+  it('geonames.hierarchy (hierarchyJSON)', function (done) {
+    // the fixtures have commas in the Lat/Long values ...
+    var lat = fixtures[1].Latitude;
+    var lng = fixtures[1].Longitude;
+    geonames.find(lat, lng, function (err, data) {
+      assert(!err, 'No erros');
+      // console.log(err, data);
+      var geonames_id = data.geonames[0].geonameId;
+      geonames.hierary(geonames_id, function (err, data) {
+        assert(!err, 'No erros');
+        // console.log(err, data);
+        assert(data.geonames[0].name === 'Earth', 'The Hotel is in Earth');
+        done();
+      });
     });
   });
 });
--- test/username.test.js
+++ test/username.test.js
@@ -7,7 +7,6 @@ process.env.GEONAMES_USERNAMES = 'user1, user2, user3, user4, user5, user6';
 
 describe('Get Random Geonames Username from List', function () {
   it('username > get_random_geonames_username', function (done) {
-    // the fixtures have commas in the Lat/Long values ...
     for (var i = 0; i < 10; i++) {
       var u = username();
       // console.log(u);
