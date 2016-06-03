b16262a26bac9d0a51c9fb72a7d0631b65838939
nelsonic <contact.nelsonic+github@gmail.com>
2016-06-03 23:14:35 +0100

    fix merge conflicts

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ var util = require('util');
  */
 exports.handler = (event, context, callback) => {
   AwsHelper.init(context, event);
+  AwsHelper.Logger(require('./package.json').name);
   var log = AwsHelper.log;
-
   log.trace({ event: util.inspect(event) }, 'Received event');
+
   // should we CHECK that the even has a location & lat/lon before lookup?
   if (!event.location || !event.location.lat || !event.location.lon) {
     var message = 'lat & lon must be set on event.location';
@@ -22,12 +23,30 @@ exports.handler = (event, context, callback) => {
   var lon = event.location.lon;
   log.info({ lat: lat, lng: lon }, 'Lat / Lng');
   geonames.find(lat, lon, (err, data) => {
-    AwsHelper.failOnError(err, event, context);
+    /* istanbul ignore if */
+    if (err) {
+      log.trace({ err: err, data: data },
+        'Geonames could not find lat: ' + lat + ' | lon: ' + lon);
+      return callback(err);
+    }
+    // AwsHelper.failOnError(err, event, context);
     var geonames_id = data.geonames[0].geonameId;
     geonames.hierarchy(geonames_id, (err, hierarchy) => {
-      AwsHelper.failOnError(err, event, context);
+      // AwsHelper.failOnError(err, event, context);
+      /* istanbul ignore if */
+      if (err) {
+        log.trace({ err: err, data: hierarchy },
+          'Geonames could not find lat: ' + lat + ' | lon: ' + lon);
+        return callback(err);
+      }
       geonames.get_all_geonames_records(hierarchy, (err, map) => {
-        AwsHelper.failOnError(err, event, context);
+        // AwsHelper.failOnError(err, event, context);
+        /* istanbul ignore if */
+        if (err) {
+          log.trace({ err: err, data: map },
+            'Geonames could not find lat: ' + lat + ' | lon: ' + lon);
+          return callback(err);
+        }
         var geo_tags = geonames.format_hierarchy_as_tags(hierarchy, map);
         log.info({ geo_tags: util.inspect(geo_tags) });
         return callback(err, geo_tags); // see: https://git.io/vwFd3
